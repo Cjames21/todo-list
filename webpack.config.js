a9cd5c24360c55fb04245b5da8a4bd778ccf7063
Charles James <charlestdjames1@gmail.com>
2022-08-18 14:34:41 -0400

    Finalizing transfer of project to new repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,21 @@
 const path = require('path');
 
- module.exports = {
-   entry: './src/index.js',
-   output: {
-     filename: 'bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-
-  module: {
-
-    rules: [
-
-      {
-
-        test: /\.css$/i,
-
-        use: ['style-loader', 'css-loader'],
-
-      },
-
-    ],
-
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
- };
+  mode: 'development',
+  module: {
+    rules: [{
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
